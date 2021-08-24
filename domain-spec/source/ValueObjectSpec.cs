194d58ab1bf5da1a459d4e9307797f8c020c43d6
Jarrod Swords <jarrod.swords@gmail.com>
2021-08-24 13:16:28 -0400

    value object structural equality

--- domain-spec/source/ValueObjectSpec.cs
+++ domain-spec/source/ValueObjectSpec.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Collections.Generic;
+using FluentAssertions;
 using Xunit;
 
 namespace SuperMarioRpg.Domain.Spec
@@ -22,19 +23,52 @@ namespace SuperMarioRpg.Domain.Spec
             valueObject2.Should().BeSameAs(valueObject1);
         }
 
+        [Fact]
+        public void WhenCheckingEquality_WithSameStructure_HasStructuralEquality()
+        {
+            var valueObject1 = Create();
+            var valueObject2 = Create();
+
+            valueObject2.Should().NotBeSameAs(valueObject1);
+            valueObject2.Should().Be(valueObject1);
+        }
+
         #endregion
 
         public class FooSpec : ValueObjectSpec
         {
             #region Protected Interface
 
-            protected override ValueObject Create() => new Foo();
+            protected override ValueObject Create() => new Foo(1);
 
             #endregion
         }
 
         private class Foo : ValueObject
         {
+            #region Creation
+
+            public Foo(int bar)
+            {
+                Bar = bar;
+            }
+
+            #endregion
+
+            #region Public Interface
+
+            public int Bar { get; }
+
+            #endregion
+
+            #region Equality
+
+            public override IEnumerable<object> GetEqualityComponents()
+            {
+                yield return Bar;
+            }
+
+            #endregion
         }
     }
 }
