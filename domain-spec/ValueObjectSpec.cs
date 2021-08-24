95f5dfe5300a1410c3c518593a842b33af4a0988
Jarrod Swords <jarrod.swords@gmail.com>
2021-08-24 12:56:27 -0400

    value object referential equality

---
+++ domain-spec/ValueObjectSpec.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace SuperMarioRpg.Domain.Spec
+{
+    public abstract class ValueObjectSpec
+    {
+        #region Protected Interface
+
+        protected abstract ValueObject Create();
+
+        #endregion
+
+        #region Test Methods
+
+        [Fact]
+        public void WhenCheckingEquality_WithSameReference_HasReferentialEquality()
+        {
+            var valueObject1 = Create();
+            var valueObject2 = valueObject1;
+
+            valueObject2.Should().BeSameAs(valueObject1);
+        }
+
+        #endregion
+
+        public class FooSpec : ValueObjectSpec
+        {
+            #region Protected Interface
+
+            protected override ValueObject Create() => new Foo();
+
+            #endregion
+        }
+
+        private class Foo : ValueObject
+        {
+        }
+    }
+}
