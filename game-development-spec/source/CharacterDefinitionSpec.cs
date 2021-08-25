62d8b9b49636a3de3180048866f4561367b23981
Jarrod Swords <jarrod.swords@gmail.com>
2021-08-25 11:42:24 -0400

    refactor assertions

--- game-development-spec/source/CharacterDefinitionSpec.cs
+++ game-development-spec/source/CharacterDefinitionSpec.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using FluentAssertions;
 using SuperMarioRpg.Domain;
 using Xunit;
@@ -9,15 +10,32 @@ namespace SuperMarioRpg.GameDevelopment.Spec
     {
         #region Core
 
-        private readonly Id _characterId;
-        private readonly ICharacterRepository _characterRepository;
+        private readonly Character _character;
 
         public CharacterDefinitionSpec()
         {
             var result = Character.Define("Mario");
             var character = ((Result<Character>) result).Value;
-            _characterId = character.Id;
-            _characterRepository = new CharacterRepository().Create(character);
+            _character = new CharacterRepository()
+                .Create(character)
+                .Find(character.Id);
+        }
+
+        #endregion
+
+        #region Private Interface
+
+        private static void Assert<T>(
+            IEventSourced aggregate,
+            params Action<T>[] conditions
+        ) where T : IEvent
+        {
+            var pendingEvents = aggregate.GetPendingEvents().ToList();
+            pendingEvents.Should().ContainSingle(x => x.GetType() == typeof(T));
+
+            var @event = pendingEvents.First(x => x.GetType() == typeof(T));
+            foreach (var condition in conditions)
+                condition.Invoke((T) @event);
         }
 
         #endregion
@@ -32,10 +50,12 @@ namespace SuperMarioRpg.GameDevelopment.Spec
             var result = Character.Define(name);
 
             var character = ((Result<Character>) result).Value;
-            var @event = (CharacterDefined) character.GetPendingEvents().Last();
 
-            @event.Name.Should().Be(name);
-            @event.Type.Should().Be(nameof(CharacterDefined));
+            Assert<CharacterDefined>(
+                _character,
+                x => x.Name.Should().Be(name),
+                x => x.Type.Should().Be(nameof(CharacterDefined))
+            );
         }
 
         [Theory]
@@ -43,14 +63,13 @@ namespace SuperMarioRpg.GameDevelopment.Spec
         [InlineData("Geno")]
         public void WhenRenaming_CharacterRenamed(string name)
         {
-            var character = _characterRepository
-                .Find(_characterId)
-                .Rename(name);
+            _character.Rename(name);
 
-            var @event = (CharacterRenamed) character.GetPendingEvents().Last();
-
-            @event.Name.Should().Be(name);
-            @event.Type.Should().Be(nameof(CharacterRenamed));
+            Assert<CharacterRenamed>(
+                _character,
+                x => x.Name.Should().Be(name),
+                x => x.Type.Should().Be(nameof(CharacterRenamed))
+            );
         }
 
         #endregion
